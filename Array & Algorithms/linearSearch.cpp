d4abc74cecff90540abfcece55bf9c5d0f214817
Navneet Mishra <155626203+Navneet-Coder-arch@users.noreply.github.com>
2024-01-15 15:35:10 +0530

    Add files via upload

---
+++ Array & Algorithms/linearSearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+// algorithm for linear search
+int linearSearch(int *arr, int n, int key){
+    int foundAtIdx = -1;
+
+    for (int i = 0; i < n; i++){
+        if (key == *(arr + i)){
+            foundAtIdx = i;
+        }
+    }
+
+    return foundAtIdx;
+
+}
+
+int main() {
+    int n; // array size
+    cout << "Size of array: ";
+    cin >> n;
+
+    int *arr = new int[n]; // array initialization
+    cout << "Elements of the array: ";
+    for (int i = 0 ; i < n; i++){
+        cin >> *(arr + i);
+    }
+
+    int key;
+    cout << "Key: ";
+    cin >> key;
+
+    cout << "Key found at index: " << linearSearch(arr, n, key); // calling linearSearch(algo)
+
+    return 0;
+}
