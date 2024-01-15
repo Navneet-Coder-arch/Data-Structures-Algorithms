bc540857615169e5a2609ae312f4384ddb3e4f35
Navneet Mishra <155626203+Navneet-Coder-arch@users.noreply.github.com>
2024-01-15 16:28:54 +0530

    Update binarySearch.cpp

--- Array & Algorithms/binarySearch.cpp
+++ Array & Algorithms/binarySearch.cpp
@@ -42,5 +42,44 @@ int main() {
 
     cout << "Key found at index: " << binarySearch(arr, n, key); // clling binarySearch(algo)
 
+    #include <iostream>
+
+using namespace std;
+
+// algorithm for linear search
+int linearSearch(int *arr, int n, int key){
+    int foundAtIdx = -1;
+
+    for (int i = 0; i < n; i++){
+        if (key == *(arr + i)){
+            foundAtIdx = i;
+        }
+    }
+
+    return foundAtIdx;
+
+}
+
+int main() {
+    int n; // array size
+    cout << "Size of array: ";
+    cin >> n;
+
+    int *arr = new int[n]; // array initialization
+    cout << "Elements of the array: ";
+    for (int i = 0 ; i < n; i++){
+        cin >> *(arr + i);
+    }
+
+    int key;
+    cout << "Key: ";
+    cin >> key;
+
+    cout << "Key found at index: " << linearSearch(arr, n, key); // calling linearSearch(algo)
+
+    //deallocation
+    delete[] arr;
+    arr = NULL;
+
     return 0;
 }
