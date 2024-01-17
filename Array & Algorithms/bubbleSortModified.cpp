c0d5b8e3297d7e18011bd04807ec5cdff226634f
Navneet Mishra <155626203+Navneet-Coder-arch@users.noreply.github.com>
2024-01-17 16:16:12 +0530

    Create bubbleSortModified.cpp

---
+++ Array & Algorithms/bubbleSortModified.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+//bubble sort algorithm
+void bubbleSort(int *arr, int n){
+
+    bool swapped = false;
+
+    for (int i = 0; i < (n - 1); i++){
+        for (int j = 0; j < (n - i) - 1; j++){
+            if (*(arr + j) > *(arr + (j + 1))){
+                swap(*(arr + j), *(arr + (j + 1)));
+                swapped = true;
+            }
+        }
+        if (swapped == false){
+            break;
+        }
+    }
+
+    return;
+
+}
+
+void displayArray(int *arr, int n){
+    cout << "Elements of the array: ";
+
+    for (int i = 0; i < n; i++){
+        cout << *(arr + i) << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Size of the array: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    cout << "Enter elements of the array: ";
+    for (int i = 0; i < n; i++){
+        cin >> *(arr + i);
+    }
+
+    bubbleSort(arr, n);
+    displayArray(arr, n);
+
+    delete[] arr;
+    arr = NULL;
+
+    return 0;
+}
